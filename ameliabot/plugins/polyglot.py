d8dc3d7f69ab07dafd7268ddb57a2aa8454d4653
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-12-01 13:43:53 -0200

    improving.

--- ameliabot/plugins/polyglot.py
+++ ameliabot/plugins/polyglot.py
@@ -1,40 +1,27 @@
 """
-Author: Iury O. G. Figueiredo.
-Name: polyglot
-Description: This plugin can be used to translate one person speech at time it is spoken on irc.
-Usage:
-<Tau>.polyglot_add pt en .
-<yu>Tau ". polyglot_add en en 189-127-54- 128.i - next.psi.br"
-<Tau>interessante.
-<yu>Tau "interesting."
-<Tau>agora posso falar portugues e alguem me traduz para o ingles.
-<yu>Tau "now I can speak Portuguese and someone translated into English ."
-
-If i just dont want it anymore.
-
-<Tau>.polyglot_rm en pt 189-127-54-128.i-next.psi.br
-
-The parameter actually is host. It is found when you do /whois on some nick.
-The pt en are the google key for portuguese and english. it accepts other keys 
-but you would have to figure it out.
 """
 
 from untwisted.network import spawn, xmap, zmap
-from ameliabot.utils.google import GoogleTranslator, shape
+from gdict import GoogleTranslator, shape
 from untwisted.plugins.irc import send_msg
+from ameliabot.cmd import command
 
 source = GoogleTranslator()
 
 def install(server):
     xmap(server, 'CMSG', split)
-    xmap(server, ('CMSG', '.polyglot_add'), add)
-    xmap(server, ('CMSG', '.polyglot_rm'), rm)
+    xmap(server, 'CMSG', add)
+    xmap(server, 'CMSG', rm)
 
-def add(server, user, lang_x, lang_y, host):
-    xmap(server, host, listen, lang_x, lang_y)
+@command('@polyglot -add lang_x lang_y ident')
+def add(server, nick, user, host, target, 
+               msg, lang_x, lang_y, ident):
+    xmap(server, ident, listen, lang_x, lang_y)
 
-def rm(server, user, lang_x, lang_y, host):
-    zmap(server, host, listen, lang_x, lang_y)
+@command('@polyglot -rm lang_x lang_y ident')
+def rm(server, nick, user, host, target, 
+               msg, lang_x, lang_y, ident):
+    zmap(server, ident, listen, lang_x, lang_y)
 
 def listen(server, nick, user, target, msg, lang_x, lang_y):
     data = source.translate(msg, lang_x, lang_y) 
@@ -43,6 +30,7 @@ def listen(server, nick, user, target, msg, lang_x, lang_y):
 
 def split(server, nick, user, host, target, msg):
     spawn(server, host, nick, user, target, msg)
+
 
 
 
