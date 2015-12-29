bd500bc7e441a55b8e3b3507d4234ceb4d34a035
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-12-29 21:09:15 -0200

    Fixing plugins.

---
+++ ameliabot/plugins/polyglot.py
@@ -0,0 +1,55 @@
+"""
+Overview
+========
+
+Used to translate all text from an user.
+
+Commands
+========
+
+Command: @polyglot-add lang_x lang_y ident
+Description: Whenever an user with ident types it translates from lang_x to lang_y.
+
+Command: @polyglot-del lang_x lang_y ident
+Description: Remove the user with ident from the list of users to have their msgs to be translated.
+"""
+
+from untwisted.network import spawn, xmap, zmap
+from libdict import GoogleTranslator
+from untwisted.plugins.irc import send_msg
+from ameliabot.cmd import command
+
+source = GoogleTranslator()
+
+def install(server):
+    xmap(server, 'CMSG', split)
+    xmap(server, 'CMSG', add)
+    xmap(server, 'CMSG', rm)
+
+@command('@polyglot-add lang_x lang_y ident')
+def add(server, nick, user, host, target, 
+               msg, lang_x, lang_y, ident):
+    xmap(server, ident, listen, lang_x, lang_y)
+
+@command('@polyglot-del lang_x lang_y ident')
+def rm(server, nick, user, host, target, 
+               msg, lang_x, lang_y, ident):
+    zmap(server, ident, listen, lang_x, lang_y)
+
+def listen(server, nick, user, target, msg, lang_x, lang_y):
+    data = source.translate(msg, lang_x, lang_y) 
+    send_msg(server, target, '%s %s' % (nick, data))
+
+def split(server, nick, user, host, target, msg):
+    spawn(server, host, nick, user, target, msg)
+
+
+
+
+
+
+
+
+
+
+
---
+++ ameliabot/plugins/translator.py
@@ -0,0 +1,39 @@
+"""
+Overview
+========
+
+Used to translate quickly some word from lang1 to lang2.
+
+Commands
+========
+
+Command: @g lang1 lang2 data
+Description: Translate the data from lang1 to lang2.
+"""
+
+from libdict import GoogleTranslator
+from untwisted.network import xmap
+from untwisted.plugins.irc import send_msg
+from ameliabot.cmd import regcmd
+
+class Translator(object):
+    def __init__(self, server):
+        self.google_translator = GoogleTranslator()
+        xmap(server, 'CMSG', self.translate)
+
+    @regcmd('@g (?P<lang1>.+?) (?P<lang2>.+?) (?P<data>.+)$')
+    def translate(self, server, nick, user, host, target, msg, lang1, lang2, data):
+        data = self.google_translator.translate(data, lang1, lang2)
+        send_msg(server, target, data)
+    
+install = Translator
+
+
+
+
+
+
+
+
+
+
