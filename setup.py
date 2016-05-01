ed529cd9611b9bbadb3c7b918e9bd073a07cbf61
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-30 21:39:46 -0400

    Fixing setup.py version.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup
 
-setup(name="vy",
-      version="0.1",
+setup(name="ameliabot",
+      version="0.0.1",
       packages=["ameliabot", 
                 "ameliabot.plugins",
                 "ameliabot.utils",
@@ -14,7 +14,14 @@ setup(name="vy",
       package_data={'ameliabot': ['ameliarc', '/ameliabot/ameliarc'],
                     'ameliabot.plugins.quote':['quote_database', '/ameliabot/plugins/quote/quote_database']},
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/ameliabot',
+      download_url='https://github.com/iogf/ameliabot/releases',
+      keywords=['irc', 'ircbot', 'untwisted', 'freenode', 'ameliabot', 'amelia'],
+      classifiers=[],
+      description="A nifty IRC Bot written on top of untwisted framework")
+
+
 
 
 
