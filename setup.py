342ea576325b23fb1f1f4618e0bdcd3f733c842d
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-30 22:36:12 -0400

    Fixing setup.py.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup
 
-setup(name="vy",
-      version="0.1",
+setup(name="ameliabot",
+      version="1.0.0",
       packages=["ameliabot", 
                 "ameliabot.plugins",
                 'ameliabot.plugins.quote'],
@@ -12,6 +12,7 @@ setup(name="vy",
                     'ameliabot.plugins.quote':['quote_database', '/ameliabot/plugins/quote/quote_database']},
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
+
 
 
 
