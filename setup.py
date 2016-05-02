57eb80bf038f43a348356e1e197b35e5c6db313b
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-02 11:47:00 -0400

    Adding description, classifies to the setup.py.

--- setup.py
+++ setup.py
@@ -11,7 +11,14 @@ setup(name="ameliabot",
       package_data={'ameliabot': ['ameliarc', '/ameliabot/ameliarc'],
                     'ameliabot.plugins.quote':['quote_database', '/ameliabot/plugins/quote/quote_database']},
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/ameliabot',
+      download_url='https://github.com/iogf/ameliabot/releases',
+      keywords=['irc', 'ircbot', 'untwisted', 'freenode', 'ameliabot', 'amelia'],
+      classifiers=[],
+      description="A nifty IRC Bot written on top of untwisted framework")
+
+
 
 
 
