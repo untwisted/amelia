f78cfb277478e4544862992d2dc64d661796a306
joo <john.macinroe@gmail.com>
2016-07-29 14:46:54 +0100

    note: fix bugs; add support for multiple messages.

--- ameliabot/plugins/note.py
+++ ameliabot/plugins/note.py
@@ -14,32 +14,63 @@ Command: @note-del peer
 Description: Remove a note from a nick.
 """
 
+from collections import namedtuple, defaultdict
+
 from untwisted.network import xmap
+
 from untwisted.plugins.irc import send_msg
-from ameliabot.cmd import command
+from ameliabot.cmd import command, regcmd
+
+MAX_NOTES_PER_PEER = 2
+MAX_NOTES = 10
+
+# source = (nick, user, host)
+Message = namedtuple('Message', ('source', 'message'))
 
 class Note(object):
     def __init__(self, server):
         xmap(server, 'CMSG', self.note_add)
-        xmap(server, 'CMSG', self.note_rm)
+        xmap(server, 'CMSG', self.note_del)
         xmap(server, 'JOIN', self.send_note)
-        self.base = {}
+        self.base = defaultdict(list)
 
-    @command('@note-add peer data')
+    @regcmd(r'@note-add\s+(?P<peer>\S+)\s+(?P<data>.*)')
     def note_add(self, server, nick, user, host, target, msg, peer, data):
-        self.base[peer.lower()] = data
+        msgs = self.base[peer.lower()]
+        user_msgs = [m for m in msgs if m.source[1:] == (user, host)
+                     or user.startswith('~') and m.source[2] == host]
+        if len(user_msgs) + 1 > MAX_NOTES_PER_PEER:
+            send_msg(server, target,
+                '%s: you may leave no more than %d notes for "%s".' % (
+                nick, MAX_NOTES_PER_PEER, peer))
+        elif len(msgs) + 1 > MAX_NOTES:
+            send_msg(server, target,
+                '%s: there are too many notes saved for "%s".' % (
+                 nick, peer))
+        else:
+            msgs.append(Message(
+                source=(nick,user,host), message=data))
+            self.base[peer.lower()] = msgs
+	    send_msg(server, target,
+		'%s: note for "%s" saved.' % (nick, peer))
 
     @command('@note-del peer')
-    def note_rm(self, server, nick, user, host, target, peer):
-        del self.base[peer.lower()]
+    def note_del(self, server, nick, user, host, target, msg, peer):
+        peer = peer.lower()
+	msgs = self.base.get(peer, [])
+	orig_len = len(msgs)
+	msgs = [m for m in msgs if m.source[1:] != (user, host)]
+        if msgs: self.base[peer] = msgs
+        elif peer in self.base: del self.base[peer]
+	send_msg(server, target or nick, '%s: %d notes deleted.' % (
+	    nick, orig_len - len(msgs)))
 
     def send_note(self, server, nick, user, host, channel):
-        try:
-            data = self.base[nick.lower()]
-        except KeyError:
-            pass
-        else:
-            send_msg(server, channel, '%s %s' % (nick, data))
+        if nick.lower() not in self.base: return
+        for msg in self.base[nick.lower()]:
+            send_msg(server, channel, '%s: <%s> %s' % (
+                nick, '%s!%s@%s' % msg.source, msg.message))
+        del self.base[nick.lower()]
 
 install = Note
 
