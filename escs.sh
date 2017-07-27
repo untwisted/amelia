c2c61f1deba24756ce1d6d79a52fc33c0415df9f
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-07-26 23:03:20 -0300

    Updating escs.

--- escs.sh
+++ escs.sh
@@ -1,75 +1,76 @@
 ##############################################################################
-# clone, ameliabot, github.
+# clone, amelia, github.
 cd ~/projects
-git clone git@github.com:iogf/ameliabot.git ameliabot-code
+git clone git@github.com:iogf/amelia.git amelia-code
 ##############################################################################
-# push, github, ameliabot.
-cd ~/projects/ameliabot-code
+# push, github, amelia.
+cd ~/projects/amelia-code
 git status
 git add *
 git commit -a
 git push
 ##############################################################################
-# create, development, branch, ameliabot. 
+# create, development, branch, amelia. 
 git branch -a
 git checkout -b development
 git push --set-upstream origin development
 ##############################################################################
-# switch, master, branch, change, ameliabot.
-cd ~/projects/ameliabot-code
+# switch, master, branch, change, amelia.
+cd ~/projects/amelia-code
 git checkout master
 ##############################################################################
-# switch, development, branch, change, ameliabot.
-cd ~/projects/ameliabot-code
+# switch, development, branch, change, amelia.
+cd ~/projects/amelia-code
 git checkout development
 ##############################################################################
-# undo, changes, ameliabot, github.
-cd ~/projects/ameliabot-code
+# undo, changes, amelia, github.
+cd ~/projects/amelia-code
 git checkout *
 ##############################################################################
-# merge, development, into master, ameliabot.
-cd ~/projects/ameliabot-code
+# merge, development, into master, amelia.
+cd ~/projects/amelia-code
 git checkout master
 git merge development
 git push
 git checkout development
 ##############################################################################
-# merge, master, into, development, ameliabot.
-cd ~/projects/ameliabot-code
+# merge, master, into, development, amelia.
+cd ~/projects/amelia-code
 git checkout development
 git merge master
 git push
 git checkout development
 ##############################################################################
-# install, ameliabot.
-cd ~/projects/ameliabot-code
+# install, amelia.
+cd ~/projects/amelia-code
 sudo bash -i
 python2 setup.py install
 rm -fr build
 exit
 ##############################################################################
-# remove, delete, .git, ameliabot.
+# remove, delete, .git, amelia.
 find . -name .git -print -exec rm -fr {} \;
 ##############################################################################
-# create, start, run, initialize, ameliarc, ameliabot.
+# create, start, run, initialize, ameliarc, amelia.
 amelia --init
 amelia --run
 ##############################################################################
-# uninstall, delete, remove, ameliabot, global, package, installation.
+# uninstall, delete, remove, amelia, global, package, installation.
 rm -fr ~/.amelia
 sudo bash -i
-rm -fr /usr/local~/projects/python2.7/dist-packages/ameliabot
+rm -fr /usr/local~/projects/python2.7/dist-packages/amelia
 exit
 ##############################################################################
-## build, make, a tarball, disutils, ameliabot.
-cd ~/projects/ameliabot-code
+## build, make, a tarball, disutils, amelia.
+cd ~/projects/amelia-code
 python2 setup.py sdist 
 rm -fr dist
 rm MANIFEST
 ##############################################################################
-# ameliabot, pip.
-cd ~/projects/ameliabot-code
+# amelia, pip.
+cd ~/projects/amelia-code
 python setup.py sdist register upload
 rm -fr dist
+
 
 
